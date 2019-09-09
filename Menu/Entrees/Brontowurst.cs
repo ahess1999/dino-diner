2413c9ffab40e9096c014115118afac2ac800344
Austin Hess <ahess@users.campus>
2019-09-09 17:14:40 -0500

    Nuggetry

---
+++ Menu/Entrees/Brontowurst.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class Brontowurst
+    {
+        private bool Bun = true;
+        private bool Peppers = true;
+        private bool Onions = true;
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Brautwurst" };
+                if (Onions) ingredients.Add("Onions");
+                if (Bun) ingredients.Add("Bun");
+                if (Peppers) ingredients.Add("Peppers");
+                return ingredients;
+            }
+        }
+
+        public Brontowurst()
+        {
+            this.Price = 5.36;
+            this.Calories = 498;
+        }
+
+        public void HoldBun()
+        {
+            this.Bun = false;
+        }
+
+        public void HoldPeppers()
+        {
+            this.Peppers = false;
+        }
+
+        public void HoldOnions()
+        {
+            this.Onions = false;
+        }
+    }
+}
