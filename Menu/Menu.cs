6649f00b3661e38fd1631c2b7c4467863b6987b1
Austin Hess <hessypoo@gmail.com>
2019-12-04 16:53:37 -0600

    Website Milestone 2 Done

--- Menu/Menu.cs
+++ Menu/Menu.cs
@@ -3,7 +3,9 @@
  */
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace DinoDiner.Menu
 {
@@ -12,6 +14,11 @@ namespace DinoDiner.Menu
     /// </summary>
     public class Menu
     {
+        /// <summary>
+        /// Hashset of possible ingredients
+        /// </summary>
+        public HashSet<string> PossibleIngredients = new HashSet<string>();
+
         /// <summary>
         /// Adds all menu items to a list
         /// </summary>
@@ -92,11 +99,145 @@ namespace DinoDiner.Menu
         /// <returns>The entire menu</returns>
         public override string ToString()
         {
-            for(int i = 0; i <= AvailableMenuItems.Count; i++)
+            for(int i = 0; i < AvailableMenuItems.Count; i++)
             {
                 return AvailableMenuItems[i].ToString() + " \n";
             }
             return "";
+        }
+
+        /// <summary>
+        /// Returns search results based on the user's search
+        /// </summary>
+        /// <param name="menuitems">List of menu items</param>
+        /// <param name="term">What is being searched for</param>
+        /// <returns>List of items that are searched for</returns>
+        public List<IMenuItem> Search(List<IMenuItem> menuitems, string term)
+        {
+            List<IMenuItem> results = new List<IMenuItem>();
+
+            foreach(IMenuItem menu in menuitems)
+            {
+                if (menu.ToString().Contains(term))
+                {
+                    results.Add(menu);
+                }
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// Filters by what the item specifically is
+        /// </summary>
+        /// <param name="menuitems">List of menu items</param>
+        /// <param name="filters">filters that the user has selected</param>
+        /// <returns>A list of combos, or entrees, or sides, or drinks</returns>
+        public List<IMenuItem> Filter(List<IMenuItem> menuitems, List<string> filters)
+        {
+            List<IMenuItem> results = new List<IMenuItem>();
+
+            foreach (IMenuItem item in menuitems)
+            {
+                if (item is CretaceousCombo && filters.Contains("Combo"))
+                {
+                    results.Add(item);
+                }
+                if (item is Entree && filters.Contains("Entree"))
+                {
+                    results.Add(item);
+                }
+                if (item is Side && filters.Contains("Side"))
+                {
+                    results.Add(item);
+                }
+                if (item is Drink && filters.Contains("Drink"))
+                {
+                    results.Add(item);
+                }
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// Filters based on ingredients
+        /// </summary>
+        /// <param name="menuitems">List of menu items to iterate through</param>
+        /// <param name="ingredients">List of all ingredients</param>
+        /// <returns>The list filtered by ingredients</returns>
+        public List<IMenuItem> FilterIngredients(List<IMenuItem> menuitems, List<string> ingredients)
+        {
+            List<IMenuItem> results = new List<IMenuItem>();
+
+            foreach (IMenuItem menu in menuitems)
+            {
+                bool Keep = true;
+                foreach(string s in ingredients)
+                {
+                    if (menu.Ingredients.Contains(s))
+                    {
+                        Keep = false;
+                        menuitems.Remove(menu);
+                    }
+                }
+                if (Keep)
+                {
+                    results.Add(menu);
+                }
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// Filters by a minimum price
+        /// </summary>
+        /// <param name="menuitems">List of items to iterate through</param>
+        /// <param name="minimumPrice">The min price decided by the user</param>
+        /// <returns>The list of items based upon the minimum price</returns>
+        public List<IMenuItem> FilterByMinPrice(List<IMenuItem> menuitems, double? minimumPrice)
+        {
+            List<IMenuItem> results = new List<IMenuItem>();
+            foreach(IMenuItem menu in menuitems)
+            {
+                if(minimumPrice <= menu.Price)
+                {
+                    results.Add(menu);
+                }
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// Filters by a maximum price
+        /// </summary>
+        /// <param name="menuitems">List of items to iterate through</param>
+        /// <param name="maximumPrice">The max price decided by the user</param>
+        /// <returns>The list of items based upon the maximum price</returns>
+        public List<IMenuItem> FilterByMaxPrice(List<IMenuItem> menuitems, double? maximumPrice)
+        {
+            List<IMenuItem> results = new List<IMenuItem>();
+            foreach (IMenuItem menu in menuitems)
+            {
+                if (maximumPrice >= menu.Price)
+                {
+                    results.Add(menu);
+                }
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// Adds to the PossibleIngredients Hashset for all ingredients
+        /// </summary>
+        /// <param name="menuitems">List of items to iterate through</param>
+        public void AllIngredients(List<IMenuItem> menuitems)
+        {
+            foreach(IMenuItem menu in menuitems)
+            {
+                foreach(string s in menu.Ingredients)
+                {
+                    PossibleIngredients.Add(s);
+                }
+            }
         }
     }
 }
--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -14,19 +14,130 @@ namespace Website.Pages
     public class MenuModel : PageModel
     {
         /// <summary>
-        /// Read-Only variable
-        /// </summary>
-        protected Menu menu;
-        /// <summary>
         /// Menu items
         /// </summary>
-        public Menu Menu { get { return menu; } }
+        public Menu Menu { get; } = new Menu();
+        /// <summary>
+        /// Represents the search
+        /// </summary>
+        [BindProperty]
+        public string Search { get; set; }
+        /// <summary>
+        /// List of menu items
+        /// </summary>
+        [BindProperty]
+        public List<IMenuItem> MenuItems { get; set; }
+        /// <summary>
+        /// List of combos
+        /// </summary>
+        [BindProperty]
+        public List<IMenuItem> Combos { get; set; }
+        /// <summary>
+        /// List of entrees
+        /// </summary>
+        [BindProperty]
+        public List<IMenuItem> Entrees { get; set; }
+        /// <summary>
+        /// List of sides
+        /// </summary>
+        [BindProperty]
+        public List<IMenuItem> Sides { get; set; }
+        /// <summary>
+        /// List of drinks
+        /// </summary>
+        [BindProperty]
+        public List<IMenuItem> Drinks { get; set; }
+        /// <summary>
+        /// Minimum price to filter by
+        /// </summary>
+        [BindProperty]
+        public double? MinimumPrice { get; set; }
+        /// <summary>
+        /// Maximum price to filter by
+        /// </summary>
+        [BindProperty]
+        public double? MaximumPrice { get; set; }
+        /// <summary>
+        /// List of things to filter by
+        /// </summary>
+        [BindProperty]
+        public List<string> Filter { get; set; } = new List<string>();
+        /// <summary>
+        /// List of ingredients
+        /// </summary>
+        [BindProperty]
+        public List<string> Ingredients { get; set; } = new List<string>();
+        /// <summary>
+        /// HashSet for the possible ingredients
+        /// </summary>
+        [BindProperty]
+        public HashSet<string> PossibleIngredientsHashSet { get; set; } = new HashSet<string>();
         /// <summary>
         /// Constructor for Menu razor page
         /// </summary>
         public void OnGet()
         {
-            menu = new Menu();
+            Combos = Menu.AvailableCombos;
+            Entrees = Menu.AvailableEntrees;
+            Sides = Menu.AvailableSides;
+            Drinks = Menu.AvailableDrinks;
+            MenuItems = Menu.AvailableMenuItems;
+            Menu.AllIngredients(Combos);
+            Menu.AllIngredients(Entrees);
+            Menu.AllIngredients(Sides);
+            Menu.AllIngredients(Drinks);
+            PossibleIngredientsHashSet = Menu.PossibleIngredients;
+        }
+        /// <summary>
+        /// Whenever something is clicked this happens
+        /// </summary>
+        public void OnPost()
+        {
+            Combos = Menu.AvailableCombos;
+            Entrees = Menu.AvailableEntrees;
+            Sides = Menu.AvailableSides;
+            MenuItems = Menu.AvailableMenuItems;
+            Drinks = Menu.AvailableDrinks;
+            Menu.AllIngredients(Combos);
+            Menu.AllIngredients(Entrees);
+            Menu.AllIngredients(Sides);
+            Menu.AllIngredients(Drinks);
+            PossibleIngredientsHashSet = Menu.PossibleIngredients;
+            if(Search != null)
+            {
+                Combos = Menu.Search(Combos, Search);
+                Entrees = Menu.Search(Entrees, Search);
+                Sides = Menu.Search(Sides, Search);
+                Drinks = Menu.Search(Drinks, Search);
+            }
+            if(Filter.Count != 0)
+            {
+                Combos = Menu.Filter(Combos, Filter);
+                Entrees = Menu.Filter(Entrees, Filter);
+                Sides = Menu.Filter(Sides, Filter);
+                Drinks = Menu.Filter(Drinks, Filter);
+            }
+            if(Ingredients.Count != 0)
+            {
+                Combos = Menu.FilterIngredients(Combos, Ingredients);
+                Entrees = Menu.FilterIngredients(Entrees, Ingredients);
+                Sides = Menu.FilterIngredients(Sides, Ingredients);
+                Drinks = Menu.FilterIngredients(Drinks, Ingredients);
+            }
+            if(MinimumPrice != null)
+            {
+                Combos = Menu.FilterByMinPrice(Combos, MinimumPrice);
+                Entrees = Menu.FilterByMinPrice(Entrees, MinimumPrice);
+                Sides = Menu.FilterByMinPrice(Sides, MinimumPrice);
+                Drinks = Menu.FilterByMinPrice(Drinks, MinimumPrice);
+            }
+            if(MaximumPrice != null)
+            {
+                Combos = Menu.FilterByMaxPrice(Combos, MaximumPrice);
+                Entrees = Menu.FilterByMaxPrice(Entrees, MaximumPrice);
+                Sides = Menu.FilterByMaxPrice(Sides, MaximumPrice);
+                Drinks = Menu.FilterByMaxPrice(Drinks, MaximumPrice);
+            }
         }
     }
 }
